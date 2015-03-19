dbf02ed62adbfda7b057a530fcddc8201d6ccbc0
Tommy Janszen <tommy.janszen@gmail.com>
2015-03-18 19:52:23 -0700

    Incorporate the ngCordova barcode scanner

--- myApp/www/templates/scan/tab-scan.js
+++ myApp/www/templates/scan/tab-scan.js
@@ -1,5 +1,29 @@
 angular.module('starter.scan-controllers', [])
 
-.controller('ScanCtrl', function($scope) {
-  console.log('made it to the ScanCtrl');
-})
+.controller('ScanCtrl', ['$scope', '$cordovaBarcodeScanner', '$cordovaDialogs', '$ionicPlatform', function($scope, $cordovaBarcodeScanner, $cordovaDialogs, $ionicPlatform) {
+
+  $scope.scanner = function(){
+    $ionicPlatform.ready(function() {
+
+      $cordovaBarcodeScanner
+       .scan()
+       .then(function(barcodeData) {
+         console.log(barcodeData);
+         // Success! Barcode data is here
+       }, function(error) {
+         // An error occurred
+       });
+
+
+     // NOTE: encoding not functioning yet
+     $cordovaBarcodeScanner
+       .encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com")
+       .then(function(success) {
+         // Success!
+       }, function(error) {
+         // An error occurred
+       });
+
+   }, false);
+  }
+}])
